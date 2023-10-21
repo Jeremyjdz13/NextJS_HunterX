62f0c115726b9670f0be0a488c1be3d7cadc66e9
Jeremyjdz13 <46872022+Jeremyjdz13@users.noreply.github.com>
2023-10-21 11:14:15 -0700

    Styles added to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,13 @@
-
+'use client'
+//Note: You can only access browser API in client component
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
+import classnames from 'classnames'
 
 const NavBar = () => {
 
+  const currentPath = usePathname()
   const links = [
     { label: 'HunterX', href: '/'},
     { label: 'Player', href: '/player'},
@@ -12,12 +16,20 @@ const NavBar = () => {
     { label: 'Sign In', href: '/auth'},
   ]
   return (
-    <nav className="flex bg-slate-200 p-3 space-x-3">
+    <nav className="flex bg-slate-700 p-3 space-x-3">
       <ul className='flex space-x-6'>
         {links.map(link => <Link
           key={link.href}
           href={link.href} 
-          className="text-zinc-800 hover:text-zinc-400 transition-colors">{link.label}</Link> 
+          className={
+            classnames({
+              'text-zinc-50': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-400': true
+            })
+          }>
+            {link.label}
+            </Link> 
         )}
       </ul>
     </nav>
