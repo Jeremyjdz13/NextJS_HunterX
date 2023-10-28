bf9d034d005ef4cea206ad9b13caa581e29f3212
Jeremyjdz13 <46872022+Jeremyjdz13@users.noreply.github.com>
2023-10-28 09:25:11 -0700

    Notes

--- app/player/notes/page.tsx
+++ app/player/notes/page.tsx
@@ -1,17 +1,38 @@
 'use client'
 import React from "react"
+import { useForm, Controller } from 'react-hook-form'
 import SimpleMDE from 'react-simplemde-editor'
 import "easymde/dist/easymde.min.css"
+import { Tag } from "../../context/NoteDataTypes" 
 
+type NoteForm = {
+  title: string
+  body: string
+  createdAt: string
+  editedAt: string
+  tags: Tag[]
+}
 const PlayerNotes = () => {
+  const {register, control, handleSubmit} = useForm<NoteForm>()
   return (
-    <div>
-      <form>
+      <form 
+        className="max-w-xl space-y-3" 
+        onSubmit={handleSubmit((data: any) => console.log(data, "Form data"))}
+      >
         <label>Title</label>
-        <SimpleMDE />
-        <button>Create</button>
+        <input 
+          placeholder="title" 
+          type="title"
+          id="title"
+          {...register('title')} 
+        />
+          <Controller 
+          name="body"
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="body" {...field}/>}          
+          />
+        <button type="submit">Create</button>
       </form>
-    </div>
   )
 }
 
